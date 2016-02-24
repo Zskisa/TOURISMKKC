1851a62a6ea63b05a481e0726fb3985ddc94ae4b
CBNUKE <jimmy29922@gmail.com>
2016-02-24 20:36:22 +0700

    Update slide

--- app/src/main/java/com/zskisa/tourismkkc/DetailFragment.java
+++ app/src/main/java/com/zskisa/tourismkkc/DetailFragment.java
@@ -7,17 +7,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.zskisa.tourismkkc.apimodel.ApiPlaces;
 
 public class DetailFragment extends Fragment {
     private View view;
     private String placesID;
+    private SliderLayout mDemoSlider;
+    private TextView txtName, txtDetail;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        txtName = (TextView) view.findViewById(R.id.text_places_name);
+        txtDetail = (TextView) view.findViewById(R.id.text_detail);
+        txtName.setText("กำลังโหลดข้อมูล");
+        txtDetail.setText("กำลังโหลดข้อมูล");
+
+        mDemoSlider = (SliderLayout) view.findViewById(R.id.slider);
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+
         Button btnReview = (Button) view.findViewById(R.id.button_review);
         btnReview.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +69,23 @@ public class DetailFragment extends Fragment {
             super.onPostExecute(apiPlaces);
             if (apiPlaces == null) {
                 Toast.makeText(getActivity(), "ผิดพลาดไม่พบสถานที่ดังกล่าว", Toast.LENGTH_LONG).show();
+            } else {
+                txtName.setText(apiPlaces.getPlaces_name());
+                txtDetail.setText(apiPlaces.getPlaces_desc());
+
+                if (apiPlaces.getPhotos() != null && apiPlaces.getPhotos().size() > 0) {
+                    int size = apiPlaces.getPhotos().size();
+                    for (int i = 0; i < size; i++) {
+                        TextSliderView textSliderView = new TextSliderView(getActivity());
+                        // initialize a SliderLayout
+                        textSliderView
+                                .image(apiPlaces.getPhotos().get(i).getUrl())
+                                .setScaleType(BaseSliderView.ScaleType.CenterCrop);
+
+                        //add your extra information
+                        mDemoSlider.addSlider(textSliderView);
+                    }
+                }
             }
         }
     }
